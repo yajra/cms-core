bd42226e01017f99d0e6d1bcde71f10bb47ac2c1
Arjay Angeles <aqangeles@gmail.com>
2016-06-16 17:47:04 +0800

    Fix theme changer middleware.

--- src/Http/Middleware/ThemeChanger.php
+++ src/Http/Middleware/ThemeChanger.php
@@ -9,17 +9,18 @@ class ThemeChanger
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
         if ($request->query('tmpl')) {
-            config()->set('theme.frontend', $request->query('tmpl'));
+            $finder   = app('theme.view.finder');
+            $basePath = config('theme.path', base_path('themes')) . DIRECTORY_SEPARATOR . $request->query('tmpl');
+            $finder->setBasePath($basePath);
         }
 
-        return $response;
+        return $next($request);
     }
 }
