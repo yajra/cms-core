ae1e1bc17846a457586ea750dbb6c9046eca4f2c
Red Redimano <gredimano@objectbright.com>
2016-06-17 08:56:24 +0800

    Update assets service provider,

--- src/Providers/AssetServiceProvider.php
+++ src/Providers/AssetServiceProvider.php
@@ -5,6 +5,11 @@ namespace Yajra\CMS\Providers;
 use Illuminate\Support\ServiceProvider;
 use Roumen\Asset\Asset;
 
+/**
+ * Class AssetServiceProvider
+ *
+ * @package Yajra\CMS\Providers
+ */
 class AssetServiceProvider extends ServiceProvider
 {
     /**
@@ -23,9 +28,25 @@ class AssetServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->addAdminAssets();
+        $this->requireDefaultAssets();
+    }
+
+    /**
+     * Add theme assets.
+     */
+    protected function addAdminAssets()
+    {
         foreach (config('site.admin-assets.' . config('site.admin-assets.default'), []) as $asset => $value) {
             Asset::add($value);
         }
+    }
+
+    /**
+     * Add require default assets.
+     */
+    protected function requireDefaultAssets()
+    {
         foreach (config('site.admin-assets.admin-required-assets', []) as $asset => $requiredValue) {
             Asset::add($requiredValue);
         }
