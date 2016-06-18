521101be493568a06e30586ffe40fdc755126d53
Arjay Angeles <aqangeles@gmail.com>
2016-06-18 23:14:41 +0800

    Do not build widgets on admin pages.

--- src/Http/Middleware/DynamicWidgetsBuilder.php
+++ src/Http/Middleware/DynamicWidgetsBuilder.php
@@ -2,10 +2,10 @@
 
 namespace Yajra\CMS\Http\Middleware;
 
-use Yajra\CMS\Entities\Widget;
 use Closure;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Collection;
+use Yajra\CMS\Entities\Widget;
 
 class DynamicWidgetsBuilder
 {
@@ -18,51 +18,67 @@ class DynamicWidgetsBuilder
      */
     public function handle($request, Closure $next)
     {
+        // skip dynamic widgets on administrator pages.
+        if ($request->is('administrator/*') || $request->is('administrator')) {
+            return $next($request);
+        }
+
         try {
-            /** @var Collection $widgets */
-            $widgets = Widget::with('permissions')->published()->get();
             $factory = app('arrilot.widget-group-collection');
-            $widgets->groupBy('position')
-                    ->sortBy('order')
-                    ->each(function (Collection $group) use ($factory) {
-                        $group->each(function (Widget $widget) use ($factory) {
-                            $displayWidget = true;
 
-                            if ($widget->requiresAuthentication() && ! auth()->check()) {
-                                $displayWidget = false;
-                            }
+            /** @var Collection $widgets */
+            $widgets = $this->getWidgets();
+            $widgets->each(function (Collection $group) use ($factory) {
+                $group->each(function (Widget $widget) use ($factory) {
+                    $displayWidget = true;
 
-                            if ($widget->permissions->count()) {
-                                if ($widget->authorization === 'can') {
-                                    foreach ($widget->permissions as $permission) {
-                                        if (auth()->guest() ||
-                                            (auth()->check() && ! auth()->user()->can($permission->slug))
-                                        ) {
-                                            $displayWidget = false;
-                                            continue;
-                                        }
-                                    }
-                                } else {
-                                    if (auth()->guest() ||
-                                        (auth()->check() && ! auth()
-                                                ->user()
-                                                ->canAtLeast($widget->permissions->pluck('slug')->toArray()))
-                                    ) {
-                                        $displayWidget = false;
-                                    }
+                    if ($widget->requiresAuthentication() && ! auth()->check()) {
+                        $displayWidget = false;
+                    }
+
+                    if ($widget->permissions->count()) {
+                        if ($widget->authorization === 'can') {
+                            foreach ($widget->permissions as $permission) {
+                                if (auth()->guest() ||
+                                    (auth()->check() && ! auth()->user()->can($permission->slug))
+                                ) {
+                                    $displayWidget = false;
+                                    continue;
                                 }
                             }
-
-                            if ($displayWidget) {
-                                $factory->group($widget->position)
-                                        ->addWidget($widget->present()->class(), [], $widget);
+                        } else {
+                            if (auth()->guest() ||
+                                (auth()->check() && ! auth()
+                                        ->user()
+                                        ->canAtLeast($widget->permissions->pluck('slug')->toArray()))
+                            ) {
+                                $displayWidget = false;
                             }
-                        });
-                    });
+                        }
+                    }
+
+                    if ($displayWidget) {
+                        $factory->group($widget->position)
+                                ->addWidget($widget->present()->class(), [], $widget);
+                    }
+                });
+            });
         } catch (QueryException $e) {
             // \\_(",)_//
         }
 
         return $next($request);
+    }
+
+    /**
+     * Get all published widgets.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getWidgets()
+    {
+        $widgets = Widget::with('permissions')->published()->get();
+
+        return $widgets->groupBy('position')->sortBy('order');
     }
 }
