15e3eb34d4e984dd0ca09b02129571ec563dfe7f
Red Redimano <gredimano@objectbright.com>
2016-06-18 17:28:29 +0800

    Update asset service provider.

--- src/Providers/ConfigurationServiceProvider.php
+++ src/Providers/ConfigurationServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace Yajra\CMS\Providers;
 
-use Yajra\CMS\Entities\Configuration;
-use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Roumen\Asset\Asset;
+use Yajra\CMS\Entities\Configuration;
+use Yajra\CMS\Entities\FileAsset;
+use Illuminate\Database\QueryException;
 
-class ConfigurationServiceProvider extends ServiceProvider
+
+/**
+ * Class AssetServiceProvider
+ *
+ * @package Yajra\CMS\Providers
+ */
+class AssetServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
-     *
-     * @return void
      */
     public function boot()
     {
         try {
-            Configuration::all()->each(function($config) {
-                config()->set($config->key, $config->value);
-            });
+            $this->addAdminAssets();
+            $this->requireAdminDefaultAssets();
+            $this->assetJs();
+            $this->assetCss();
         } catch (QueryException $e) {
             // \\_(",)_//
         }
@@ -31,6 +39,75 @@ class ConfigurationServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+    }
+
+    /**
+     * Load admin assets.
+     */
+    protected function addAdminAssets()
+    {
+        foreach (config('asset.admin_assets') as $asset) {
+            Asset::add($this->getAssetUrlByname($asset)->url);
+        }
+    }
+
+    /**
+     * Load require admin default assets.
+     */
+    protected function requireAdminDefaultAssets()
+    {
+        foreach (config('asset.admin_required_assets', []) as $asset => $requiredValue) {
+            Asset::add($requiredValue);
+        }
+    }
+
+    /**
+     * Load js assets.
+     *
+     * @return string
+     */
+    protected function assetJs()
+    {
+        Blade::directive('assetJs', function ($asset) {
+            $asset = $this->getAssetUrlByname($this->strParser($asset . '.js'));
+
+            return '<?php echo "<script src=\"' . $asset->url . '\"></script>"; ?>';
+        });
+    }
+
+    /**
+     * Load css assets.
+     *
+     * @return string
+     */
+    protected function assetCss()
+    {
+        Blade::directive('assetCss', function ($asset) {
+            $asset = $this->getAssetUrlByname($this->strParser($asset . '.css'));
+
+            return '<?php echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"' . $asset->url . '\">"; ?>';
+        });
+    }
+
+    /**
+     * @param string $str
+     * @return string
+     */
+    private function strParser($str)
+    {
+        return str_replace("'", '', str_replace(['(', ')'], '', $str));
+    }
+
+    /**
+     * Get url by asset name.
+     *
+     * @param string $asset
+     * @return FileAsset
+     */
+    private function getAssetUrlByname($asset)
+    {
+        return FileAsset::where('name', $asset)
+                        ->where('category', Configuration::key('asset.default'))
+                        ->first();
     }
 }
