820635c941b4792985b62c8824080e49d00f1ab3
Red Redimano <gredimano@objectbright.com>
2016-06-18 13:12:01 +0800

    Add new file asset model.

---
+++ src/Entities/FileAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yajra\CMS\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FileAsset extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'file_assets';
+}
