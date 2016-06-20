2d255f4de42b9c5eee606739286615ae3052c20c
Red Redimano <gredimano@objectbright.com>
2016-06-21 07:12:21 +0800

    Update asset mgmt form.

--- src/resources/views/administrator/configuration/partials/form/asset-management.blade.php
+++ src/resources/views/administrator/configuration/partials/form/asset-management.blade.php
@@ -16,46 +16,37 @@
 
 <div class="row">
     <div class="col-md-12">
-        <div class="box box-default">
-            <div class="box-header with-border">
-                <label class="box-title form-label-style font-s14">Site Assets
-                    @tooltip('Some text here.')
-                </label>
-            </div><!-- /.box-header -->
-            <div class="box-body" style="padding: 0px">
-                <div class="nav-tabs-custom">
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#css" data-toggle="tab">
-                                <span class="label label-success">CSS</span>
-                            </a></li>
-                        <li><a href="#javascript" data-toggle="tab" @click="showJsAssets()">
-                            <span class="label label-warning">Javascript</span>
-                            </a></li>
-                    </ul>
-                    <div class="tab-content">
-                        <div class="active tab-pane" id="css">
-                            <table class="table table-hover" id="css-assets-table" style="margin-top: 3px !important;">
-                                <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>URL</th>
-                                </tr>
-                                </thead>
-                            </table>
-                        </div><!-- /.tab-pane -->
-                        <div class="tab-pane" id="javascript">
-                            <table class="table table-hover" id="js-assets-table" style="margin-top: 3px !important;">
-                                <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>URL</th>
-                                </tr>
-                                </thead>
-                            </table>
-                        </div><!-- /.tab-pane -->
-                    </div><!-- /.tab-content -->
-                </div>
-            </div><!-- /.box-body -->
+        <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#css" data-toggle="tab">
+                        <span class="label label-success">CSS</span>
+                    </a></li>
+                <li><a href="#javascript" data-toggle="tab" @click="showJsAssets()">
+                    <span class="label label-warning">Javascript</span>
+                    </a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="active tab-pane" id="css">
+                    <table class="table table-hover" id="css-assets-table" style="margin-top: 3px !important;">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>URL</th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div><!-- /.tab-pane -->
+                <div class="tab-pane" id="javascript">
+                    <table class="table table-hover" id="js-assets-table" style="margin-top: 3px !important;">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>URL</th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div><!-- /.tab-pane -->
+            </div><!-- /.tab-content -->
         </div>
     </div>
 </div>
