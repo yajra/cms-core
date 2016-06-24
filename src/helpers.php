9348e1824abf39c4709f90d4bf6f111002ca47f9
Arjay Angeles <aqangeles@gmail.com>
2016-06-24 16:52:36 +0800

    Move theme asset to themes package.

--- src/helpers.php
+++ src/helpers.php
@@ -117,22 +117,6 @@ if (! function_exists('form')) {
     }
 }
 
-if (! function_exists('theme_asset')) {
-    /**
-     * Get theme public path.
-     *
-     * @param string $path
-     * @param bool $secure
-     * @return string
-     */
-    function theme_asset($path = '', $secure = false)
-    {
-        $path = $path ? DIRECTORY_SEPARATOR . $path : $path;
-
-        return asset('themes' . DIRECTORY_SEPARATOR . config('theme.frontend', 'default') . $path, $secure);
-    }
-}
-
 if (! function_exists('bytesToHuman')) {
     /**
      * Convert bytes to a human readable presentation.
