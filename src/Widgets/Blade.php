e628751e22a82805f3de563d8260cd0c505c8730
Arjay Angeles <aqangeles@gmail.com>
2016-06-29 23:16:29 +0800

    Add blade view widget.

---
+++ src/Widgets/Blade.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Yajra\CMS\Widgets;
+
+use Arrilot\Widgets\AbstractWidget;
+
+class Blade extends AbstractWidget
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     *
+     * @param \Yajra\CMS\Entities\Widget $widget
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function run($widget)
+    {
+        return view($widget->present()->template(), [
+            'config' => $this->config,
+            'widget' => $widget,
+        ]);
+    }
+}
