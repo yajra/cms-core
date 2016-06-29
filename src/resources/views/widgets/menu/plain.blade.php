b8268f2a55b04896bedbb698d5aedd6206d2f4ac
Arjay Angeles <aqangeles@gmail.com>
2016-06-29 23:05:08 +0800

    Add breadcrumb widget and manifest file.

---
+++ src/resources/views/widgets/menu/plain.blade.php
@@ -0,0 +1,3 @@
+<ul class="{{$widget->param('menu_class', 'nav navbar-nav')}}">
+    @include('system.menu.bootstrap', ['menu' => $$menu])
+</ul>
--- src/resources/views/widgets/partials/none.blade.php
+++
@@ -1,1 +0,0 @@
-<div class="alert alert-info">No custom parameters form found for this widget!</div>
