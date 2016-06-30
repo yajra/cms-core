818faa901fba4e3bd72d9162db4f0656eee8475a
Arjay Angeles <aqangeles@gmail.com>
2016-06-30 08:14:20 +0800

    Add widget title on plain template.

--- src/resources/views/widgets/menu/plain.blade.php
+++ src/resources/views/widgets/menu/plain.blade.php
@@ -1,3 +1,6 @@
+@if($widget->show_title)
+<h3 @if($widget->param('header_class'))class="{{$widget->param('header_class')}}"@endif>{{$widget->title}}</h3>
+@endif
 <ul class="{{$widget->param('menu_class', 'nav navbar-nav')}}">
     @include('system.menu.bootstrap', ['menu' => $$menu])
 </ul>
