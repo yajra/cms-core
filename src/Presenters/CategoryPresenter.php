5ab76ee21a75a7d8d47aa86b17fa694342834855
Arjay Angeles <aqangeles@gmail.com>
2016-07-19 23:17:45 +0800

    Extract pad to param and remove space.

--- src/Presenters/CategoryPresenter.php
+++ src/Presenters/CategoryPresenter.php
@@ -9,10 +9,11 @@ class CategoryPresenter extends Presenter
     /**
      * Indented title against depth.
      *
+     * @param int $pad
      * @return string
      */
-    public function indentedTitle()
+    public function indentedTitle($pad = 1)
     {
-        return str_repeat('— ', $this->entity->depth - 1) . ' ' . $this->entity->title;
+        return str_repeat('— ', $this->entity->depth - $pad) . $this->entity->title;
     }
 }
