a8c6faf4e07164b6288e28e62d245ce8459bfde3
Arjay Angeles <aqangeles@gmail.com>
2016-07-20 10:10:19 +0800

    Fix import.

--- src/Http/Controllers/FileAssetController.php
+++ src/Http/Controllers/FileAssetController.php
@@ -2,7 +2,6 @@
 
 namespace Yajra\CMS\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Yajra\CMS\DataTables\FileAssetsDataTable;
 use Yajra\CMS\Entities\FileAsset;
