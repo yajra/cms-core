1ef7266238f733326b835807540e4a6045462c52
Arjay Angeles <aqangeles@gmail.com>
2016-07-25 10:43:59 +0800

    Fix menu url extension.

--- src/resources/views/administrator/navigation/menu/partials/extensions/url/external.blade.php
+++ src/resources/views/administrator/navigation/menu/partials/extensions/url/external.blade.php
@@ -9,7 +9,7 @@
         </label>
         <div class="input-group {!! $errors->has('url') ? 'has-error' : '' !!}">
             <span class="input-group-addon site-label"><i class="fa fa-globe"></i></span>
-            {!! form()->input('text', 'url', null, ['id'=>'url','class'=>'form-control','placeholder'=>'URL']) !!}
+            {!! form()->input('text', 'url', old('url', $menu->url), ['id'=>'url','class'=>'form-control','placeholder'=>'URL']) !!}
         </div>
         {!! $errors->first('url', '<span class="help-block" style="color:#dd4b39">:message</span>') !!}
     </div>
