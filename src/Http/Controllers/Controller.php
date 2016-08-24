f2dd08e5e8122e470ae9860f5b8aa72026ab61d3
Arjay Angeles <aqangeles@gmail.com>
2016-08-24 17:03:45 +0800

    Revert 5.2 controller.

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,15 +2,16 @@
 
 namespace Yajra\CMS\Http\Controllers;
 
+use Yajra\CMS\Http\NotificationResponse;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Yajra\Acl\Traits\AuthorizesPermissionResources;
-use Yajra\CMS\Http\NotificationResponse;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    use AuthorizesPermissionResources, NotificationResponse;
+    use AuthorizesRequests, AuthorizesResources, AuthorizesPermissionResources,
+        DispatchesJobs, ValidatesRequests, NotificationResponse;
 }
