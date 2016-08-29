3d12d3ae97457ff4d54116adbb050652432f00b0
Arjay Angeles <aqangeles@gmail.com>
2016-08-29 13:48:45 +0800

    Fix menu items routes.

--- src/Http/routes/navigation.php
+++ src/Http/routes/navigation.php
@@ -14,12 +14,12 @@ $router->get('navigation/menu/extensions/{menu}', MenuItemsController::class . '
 $router->post('navigation/{navigation}/menu/{menu}/publish', MenuItemsController::class . '@publish')
        ->name('administrator.navigation.menu.publish');
 $router->get('navigation/menu/articles', MenuItemsController::class . '@articles');
-$router->get('navigation/{navigation}/menu', NavigationController::class . '@index')->name('administrator.navigation.menu.index');
-$router->get('navigation/{navigation}/menu/create', NavigationController::class . '@create')->name('administrator.navigation.menu.create');
-$router->post('navigation/{navigation}/menu', NavigationController::class . '@store')->name('administrator.navigation.menu.store');
-$router->get('navigation/{navigation}/menu/{menu}/edit', NavigationController::class . '@edit')->name('administrator.navigation.menu.edit');
-$router->put('navigation/{navigation}/menu/{menu}', NavigationController::class . '@update')->name('administrator.navigation.menu.update');
-$router->delete('navigation/{navigation}/menu/{menu}', NavigationController::class . '@destroy')->name('administrator.navigation.menu.destroy');
+$router->get('navigation/{navigation}/menu', MenuItemsController::class . '@index')->name('administrator.navigation.menu.index');
+$router->get('navigation/{navigation}/menu/create', MenuItemsController::class . '@create')->name('administrator.navigation.menu.create');
+$router->post('navigation/{navigation}/menu', MenuItemsController::class . '@store')->name('administrator.navigation.menu.store');
+$router->get('navigation/{navigation}/menu/{menu}/edit', MenuItemsController::class . '@edit')->name('administrator.navigation.menu.edit');
+$router->put('navigation/{navigation}/menu/{menu}', MenuItemsController::class . '@update')->name('administrator.navigation.menu.update');
+$router->delete('navigation/{navigation}/menu/{menu}', MenuItemsController::class . '@destroy')->name('administrator.navigation.menu.destroy');
 
 // Navigation Routes
 $router->post('navigation/{navigation}/publish', NavigationController::class . '@publish')
