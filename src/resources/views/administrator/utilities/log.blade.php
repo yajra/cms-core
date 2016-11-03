d96aec68f62df11af7234b72f4b9826498d91e94
Red Redimano <gredimano@objectbright.com>
2016-11-03 16:42:28 +0800

    Fix button label.

--- src/resources/views/administrator/utilities/log.blade.php
+++ src/resources/views/administrator/utilities/log.blade.php
@@ -91,7 +91,7 @@
     				<div id="stack-details"></div>
     			</div>
     			<div class="modal-footer">
-    				<button type="button" class="btn btn-default" data-dismiss="modal">{{trans('button.close')}}</button>
+    				<button type="button" class="btn btn-default" data-dismiss="modal">{{trans('cms::button.close')}}</button>
     			</div>
     		</div>
     	</div>
