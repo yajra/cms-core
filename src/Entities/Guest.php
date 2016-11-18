39cc36276f826716ebf8bcdfc7325a0db721777b
Arjay Angeles <aqangeles@gmail.com>
2016-11-18 08:30:38 +0800

    Fix roles relation.

--- src/Entities/Guest.php
+++ src/Entities/Guest.php
@@ -15,4 +15,14 @@ class Guest extends User
      * @var string
      */
     protected $primaryKey = 'id';
+
+    /**
+     * Model can have many roles.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(config('acl.role', Role::class), 'role_user', 'user_id', 'role_id')->withTimestamps();
+    }
 }
