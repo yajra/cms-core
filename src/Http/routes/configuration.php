e7cf2af8d358b2bcbd522dcc889299fefc66688e
Red Redimano <red.redimano@live.com>
2016-12-05 14:16:38 +0800

    Fix route configuration: remove file asset controller.

--- src/Http/routes/configuration.php
+++ src/Http/routes/configuration.php
@@ -5,12 +5,6 @@
  * @var \Illuminate\Routing\Router $router
  */
 
-use Yajra\CMS\Http\Controllers\FileAssetController;
 use Yajra\CMS\Http\Controllers\SiteConfigurationController;
 
-$router->get('configuration/assets/{category}', FileAssetController::class . '@getAssets');
-$router->post('configuration/asset/store', FileAssetController::class . '@storeAsset');
-$router->get('configuration/asset/edit/{asset}', FileAssetController::class . '@editAsset');
-$router->post('configuration/asset/update/{asset}', FileAssetController::class . '@updateAsset');
-$router->post('configuration/asset/delete/{asset}', FileAssetController::class . '@deleteAsset');
 $router->resource('configuration', SiteConfigurationController::class);
