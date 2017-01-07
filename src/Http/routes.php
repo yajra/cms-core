b547f5c7f5a8ecb5ecd4b1ac6c686de7c5cb1079
Arjay Angeles <aqangeles@gmail.com>
2017-01-07 13:40:33 +0800

    Use require to fix route issue on unit testing.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -11,16 +11,16 @@ use Yajra\CMS\Http\Controllers\DashboardController;
 
 $router->get('/', DashboardController::class . '@index')->name('index');
 
-require_once 'routes/configuration.php';
-require_once 'routes/navigation.php';
-require_once 'routes/utilities.php';
-require_once 'routes/extension.php';
-require_once 'routes/content.php';
-require_once 'routes/widgets.php';
-require_once 'routes/modules.php';
-require_once 'routes/profile.php';
-require_once 'routes/media.php';
-require_once 'routes/acl.php';
+require 'routes/configuration.php';
+require 'routes/navigation.php';
+require 'routes/utilities.php';
+require 'routes/extension.php';
+require 'routes/content.php';
+require 'routes/widgets.php';
+require 'routes/modules.php';
+require 'routes/profile.php';
+require 'routes/media.php';
+require 'routes/acl.php';
 
 $router->get('abort/{code}', function ($code) {
     abort($code, "$code simulation");
