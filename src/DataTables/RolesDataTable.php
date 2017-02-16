3ce6254666cce133b5be0fbdcf51af2e069a332a
Arjay Angeles <aqangeles@gmail.com>
2017-02-16 14:22:19 +0800

    Fix Roles DT scoping. Upgrade DT v7.2 and Buttons 1.1.

--- src/DataTables/RolesDataTable.php
+++ src/DataTables/RolesDataTable.php
@@ -8,32 +8,11 @@ use Yajra\Datatables\Services\DataTable;
 class RolesDataTable extends DataTable
 {
     /**
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function ajax()
-    {
-        return $this->datatables
-            ->eloquent($this->query())
-            ->editColumn('system', function (Role $role) {
-                return dt_check($role->system);
-            })
-            ->addColumn('users', function (Role $role) {
-                return view('administrator.roles.datatables.users', compact('role'))->render();
-            })
-            ->addColumn('permissions', function (Role $role) {
-                return view('administrator.roles.datatables.permissions', compact('role'))->render();
-            })
-            ->addColumn('action', 'administrator.roles.datatables.action')
-            ->rawColumns(['system', 'users', 'permissions', 'action'])
-            ->make(true);
-    }
-
-    /**
      * @return \Illuminate\Database\Eloquent\Builder
      */
     public function query()
     {
-        return Role::query();
+        return $this->applyScopes(Role::query());
     }
 
     /**
@@ -68,5 +47,27 @@ class RolesDataTable extends DataTable
                     'reload',
                 ],
             ]);
+    }
+
+    /**
+     * Build DataTable api response.
+     *
+     * @return \Yajra\Datatables\Engines\BaseEngine
+     */
+    protected function dataTable()
+    {
+        return $this->datatables
+            ->eloquent($this->query())
+            ->editColumn('system', function (Role $role) {
+                return dt_check($role->system);
+            })
+            ->addColumn('users', function (Role $role) {
+                return view('administrator.roles.datatables.users', compact('role'))->render();
+            })
+            ->addColumn('permissions', function (Role $role) {
+                return view('administrator.roles.datatables.permissions', compact('role'))->render();
+            })
+            ->addColumn('action', 'administrator.roles.datatables.action')
+            ->rawColumns(['system', 'users', 'permissions', 'action']);
     }
 }
