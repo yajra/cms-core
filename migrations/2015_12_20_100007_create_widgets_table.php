773665769c52527fcaa19a5015be3bfeaeb9f530
Arjay Angeles <aqangeles@gmail.com>
2017-05-06 16:27:43 +0800

    Update position from 20 to 50 chars. Remove transaction closure.

--- migrations/2015_12_20_100007_create_widgets_table.php
+++ migrations/2015_12_20_100007_create_widgets_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateWidgetsTable extends Migration
@@ -14,42 +13,40 @@ class CreateWidgetsTable extends Migration
      */
     public function up()
     {
-        DB::transaction(function () {
-            Schema::create('widgets', function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedInteger('extension_id')->index();
-                $table->string('title')->index();
-                $table->string('position', 20)->index();
-                $table->string('template', 100)->default('default')->index();
-                $table->string('custom_template', 100)->nullable()->index();
-                $table->unsignedSmallInteger('order')->default(1);
-                $table->boolean('published')->default(true)->index();
-                $table->boolean('show_title')->default(true)->index();
-                $table->boolean('authenticated')->default(false)->index();
-                $table->text('body')->nullable();
-                $table->text('parameters')->nullable();
-                $table->string('authorization', 20)->default('can');
-                $table->unsignedInteger('created_by')->nullable()->index();
-                $table->unsignedInteger('updated_by')->nullable()->index();
-                $table->timestamps();
+        Schema::create('widgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('extension_id')->index();
+            $table->string('title')->index();
+            $table->string('position', 50)->index();
+            $table->string('template', 100)->default('default')->index();
+            $table->string('custom_template', 100)->nullable()->index();
+            $table->unsignedSmallInteger('order')->default(1);
+            $table->boolean('published')->default(true)->index();
+            $table->boolean('show_title')->default(true);
+            $table->boolean('authenticated')->default(false);
+            $table->text('body')->nullable();
+            $table->text('parameters')->nullable();
+            $table->string('authorization', 20)->default('can');
+            $table->unsignedInteger('created_by')->nullable()->index();
+            $table->unsignedInteger('updated_by')->nullable()->index();
+            $table->timestamps();
 
-                $table->foreign('extension_id')->references('id')->on('extensions');
-            });
+            $table->foreign('extension_id')->references('id')->on('extensions');
+        });
 
-            Schema::create('widget_permission', function (Blueprint $table) {
-                $table->unsignedInteger('widget_id')->index();
-                $table->unsignedInteger('permission_id')->index();
+        Schema::create('widget_permission', function (Blueprint $table) {
+            $table->unsignedInteger('widget_id')->index();
+            $table->unsignedInteger('permission_id')->index();
 
-                $table->foreign('widget_id')->references('id')->on('widgets')->onDelete('cascade');
-                $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
-            });
+            $table->foreign('widget_id')->references('id')->on('widgets')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+        });
 
-            Schema::create('widget_menu', function (Blueprint $table) {
-                $table->unsignedInteger('widget_id')->index();
-                $table->unsignedInteger('menu_id')->index();
+        Schema::create('widget_menu', function (Blueprint $table) {
+            $table->unsignedInteger('widget_id')->index();
+            $table->unsignedInteger('menu_id')->index();
 
-                $table->foreign('widget_id')->references('id')->on('widgets')->onDelete('cascade');
-            });
+            $table->foreign('widget_id')->references('id')->on('widgets')->onDelete('cascade');
         });
     }
 
@@ -60,10 +57,8 @@ class CreateWidgetsTable extends Migration
      */
     public function down()
     {
-        DB::transaction(function () {
-            Schema::drop('widget_menu');
-            Schema::drop('widget_permission');
-            Schema::drop('widgets');
-        });
+        Schema::drop('widget_menu');
+        Schema::drop('widget_permission');
+        Schema::drop('widgets');
     }
 }
