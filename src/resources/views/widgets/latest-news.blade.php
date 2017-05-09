eca8079bd4bb3cdb0e8f24d1847b8be9c277fb6d
Arjay Angeles <aqangeles@gmail.com>
2017-05-09 19:51:28 +0800

    Display latest news on row container.

--- src/resources/views/widgets/latest-news.blade.php
+++ src/resources/views/widgets/latest-news.blade.php
@@ -1,6 +1,11 @@
-<ul class="list-group">
-    <li class="list-group-item list-group-item-heading"><i class="fa fa-newspaper-o"></i> {{$widget->title}}</li>
-    @foreach($articles as $article)
-        <li class="list-group-item"><a href="{{$article->getUrl()}}">{{str_limit($article->title, 30)}}</a></li>
-    @endforeach
-</ul>
+<div class="row">
+    <div class="col-xs-12">
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-heading"><i class="fa fa-newspaper-o"></i> {{$widget->title}}
+            </li>
+            @foreach($articles as $article)
+                <li class="list-group-item"><a href="{{$article->getUrl()}}">{{str_limit($article->title, 30)}}</a></li>
+            @endforeach
+        </ul>
+    </div>
+</div>
