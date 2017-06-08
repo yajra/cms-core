f4d53b632453a55b5a4a2583c1d7a91a66e66c66
Arjay Angeles <aqangeles@gmail.com>
2017-06-08 11:32:16 +0800

    Add empty articles handler for widgets.

--- src/resources/views/widgets/latest-news.blade.php
+++ src/resources/views/widgets/latest-news.blade.php
@@ -3,9 +3,11 @@
         <ul class="list-group">
             <li class="list-group-item list-group-item-heading"><i class="fa fa-newspaper-o"></i> {{$widget->title}}
             </li>
-            @foreach($articles as $article)
-                <li class="list-group-item"><a href="{{$article->getUrl()}}">{{str_limit($article->title, 30)}}</a></li>
-            @endforeach
+            @forelse($articles as $article)
+                <li><a href="{{$article->getUrl()}}">{{str_limit($article->title, 60)}}</a></li>
+            @empty
+                <p>WHOOPS, THERE ARE NO ARTICLES HERE YET :/</p>
+            @endforelse
         </ul>
     </div>
 </div>
--- src/resources/views/widgets/popular-articles.blade.php
+++ src/resources/views/widgets/popular-articles.blade.php
@@ -3,14 +3,16 @@
         <ul class="list-group">
             <li class="list-group-item list-group-item-heading"><i class="fa fa-newspaper-o"></i> {{$widget->title}}
             </li>
-            @foreach($articles as $article)
+            @forelse($articles as $article)
                 <li class="list-group-item">
                     <span class="label label-success"><i class="fa fa-eye"></i> {{$article->hits}}</span>
                     <a href="{{$article->getUrl()}}">
                         {{str_limit($article->title, 25)}}
                     </a>
                 </li>
-            @endforeach
+            @empty
+                <p>WHOOPS, THERE ARE NO ARTICLES HERE YET :/</p>
+            @endforelse
         </ul>
     </div>
 </div>
