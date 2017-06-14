8b740331845645d4b01595ba8fcd036d0665973c
Arjay Angeles <aqangeles@gmail.com>
2017-06-14 10:12:41 +0800

    Add search breadcrumbs.

--- src/Http/routes-frontend.php
+++ src/Http/routes-frontend.php
@@ -1,5 +1,17 @@
 <?php
 
 use Yajra\CMS\Http\Controllers\SearchController;
+use Yajra\Breadcrumbs\Generator;
 
+/**
+ * Register site specific routes.
+ */
 Route::get('search', SearchController::class . '@show')->name('search');
+
+/**
+ * Register breadcrumbs.
+ */
+Breadcrumbs::register('site.search', function (Generator $breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Search', route('site.search'));
+});
