e2bfdb3593b6bcc7311e8bef1d1cab19a1d1bd28
Arjay Angeles <aqangeles@gmail.com>
2018-04-28 17:13:42 +0800

    Fix article meta.

--- src/resources/views/article/show.blade.php
+++ src/resources/views/article/show.blade.php
@@ -1,25 +1,11 @@
 @extends('layouts.' . $template)
 
-@section('body', '')
-
-@push('styles')
-@endpush
-
+@section('title', $article->title)
 @section('keywords', $article->param('meta_keywords') ?? config('site.keywords'))
 @section('description', $article->param('meta_description') ?? config('site.description'))
 @section('author', $article->present()->author ?? config('site.author'))
-
-@section('title')
-{{ $article->title }} | @parent
-@stop
-
-@section('page-title')
-{{$article->title}}
-@stop
+@section('page-title', $article->title)
 
 @section('content')
     @include('article.blog', ['article' => $article])
 @stop
-
-@push('scripts')
-@endpush
