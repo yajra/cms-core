e498af9c343a40a6ee0f091ca30648038cf6e488
Arjay Angeles <aqangeles@gmail.com>
2018-05-30 10:42:29 +0800

    Fix navigation form tooltip.

--- src/resources/views/administrator/navigation/partials/form.blade.php
+++ src/resources/views/administrator/navigation/partials/form.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{ hasError('title') }}">
     <label class="form-label-style block" for="title">
         {{trans('cms::navigation.field.title')}}
-        @tooltip('navigation.tooltip.title')
+        @tooltip('cms::navigation.tooltip.title')
     </label>
     {!! form()->input('text', 'title', null, ['id'=>'title','class'=>'form-control','placeholder'=>trans('cms::navigation.field.title_placeholder')]) !!}
     @error('title')
@@ -10,7 +10,7 @@
 <div class="form-group {{ hasError('type') }}">
     <label class="form-label-style block" for="type">
         {{trans('cms::navigation.field.type')}}
-        @tooltip('navigation.tooltip.type')
+        @tooltip('cms::navigation.tooltip.type')
     </label>
     {!! form()->input('text', 'type', null, ['id'=>'type','class'=>'form-control','placeholder'=>trans('cms::navigation.field.type_placeholder')]) !!}
     @error('type')
@@ -19,7 +19,7 @@
 <div class="form-group {{ hasError('published') }}">
     <label class="form-label-style" for="published">
         {{trans('cms::navigation.field.published')}}
-        @tooltip('navigation.tooltip.published')
+        @tooltip('cms::navigation.tooltip.published')
     </label>
     <br>
     {!! form()->checkbox('published', $value = 1, $checked = null, ['id'=>'published','class'=>'form-control bootstrap-checkbox']) !!}
