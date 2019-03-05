9a96dd2d9dbf40fc9c77f2a608bf7f1a87c9ce33
Arjay Q. Angeles <aqangeles@gmail.com>
2019-03-05 16:53:40 +0800

    Use null coalescence operator.

--- src/resources/views/administrator/partials/icon-required.blade.php
+++ src/resources/views/administrator/partials/icon-required.blade.php
@@ -1,6 +1,6 @@
 <small class="pull-right font-s8"
        data-toggle="tooltip"
-       data-title="{{$tooltip or 'This field is required!'}}"
+       data-title="{{$tooltip ?? 'This field is required!'}}"
        data-container="body"
 >
     <i class="fa fa-asterisk"></i>
